ff395a8fd8a9372997989ae24a3448ddd69434c8
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-09-13 20:34:46 +0200

    Update baseapi

--- config/app.php
+++ config/app.php
@@ -11,11 +11,11 @@ return [
         'host' => $_ENV['APP_HOST'] ?? '127.0.0.1',
         'port' => $_ENV['APP_PORT'] ?? 7879,
     ],
-    
+
     'cors' => [
-        'allowlist' => explode(',', $_ENV['CORS_ALLOWED_ORIGINS'] ?? 'http://127.0.0.1:5173,http://localhost:5173'),
+        'allowlist' => explode(',', $_ENV['CORS_ALLOWLIST'] ?? 'http://127.0.0.1:5173,http://localhost:5173'),
     ],
-    
+
     'database' => [
         'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
         'port' => $_ENV['DB_PORT'] ?? 7878,
